2de5f11f490eb84775c23994a1aac607f44d485f
lahirusb97 <lahirushirandev@gmail.com>
2025-03-16 18:46:51 +0530

    update

--- mypos/mypos/settings.py
+++ mypos/mypos/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(','
 
 
 CORS_ALLOW_CREDENTIALS = True 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='localhost,127.0.0.1').split(',')
 CORS_ALLOW_METHODS = [
     "GET",
     "POST",
